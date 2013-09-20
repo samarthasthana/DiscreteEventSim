95790214644e5f00accc17c375662460eb15e336
samarthasthana <samarth.asthana@gmail.com>
2013-09-20 00:00:19 -0400

    Wrote simulation logic and statistics capture

--- main.cpp
+++ main.cpp
@@ -19,9 +19,6 @@ using namespace std;
 class Node{
 private:
     int packno;
-    float iat;// inter arrival time 
-    float srt;// service time 
-    float qin,qout,sysin;// queue in time , queue out time , system out time
     Node* next;
 public:
     Node(){
@@ -122,8 +119,76 @@ public:
 
 int main(int argc, char** argv) {
 
+    float times=0.00; // initialize the time to zero
     Queue q1;
-
+    int packet_max=10000;
+    float lam1,lam2;
+    int packet=0; // initialize the packet number to zero
+    int popped;
+    float lastsysout=0.0,sim_time=0.0;
+    int server=0; // 0 signifies available and 1 says not available
+    float* iat;
+    float* srt;
+    float* qin;
+    float *qout,*sysout;// create dynamic arrays for inter arrival time 
+                                  // service time , queue in and out, system in and out
+    cout<<"Enter the simulation time"<<endl;
+    cin>>sim_time;
+    cout<<"Enter the mean for inter arrival time distribution"<<endl;
+    cin>>lam1;
+    cout<<"Enter the mean for service time distribution"<<endl;
+    cin>>lam2;
+    packet_max++; // to accomodate for starting at 1 rather than 0
+    iat= new float[packet_max];
+    srt= new float[packet_max];
+    qin= new float[packet_max];
+    qout= new float[packet_max];
+    sysout= new float[packet_max];
+    packet_max--;
+    srand (time(NULL));
+    while(times<=sim_time){
+        packet++;
+        iat[packet]=genExpo(lam1);
+        times=times+iat[packet];
+        srt[packet]=genExpo(lam2);
+        if(server==1){
+            if(lastsysout<=times){
+                server=0;
+            }
+        }
+        if(q1.lengthQueue()==0 ){            
+            if(server==0){ //both the queue and the server are free.
+            qin[packet]=times;            
+            qout[packet]=times;            
+            sysout[packet]=qout[packet]+srt[packet];
+            lastsysout=sysout[packet];
+            server=1;
+            }
+            else{ // queue is free ,server is not free               
+               q1.pushQueue(packet);
+               qin[packet]=times;
+            }
+        }
+        else{ // queue is not empty
+            if(server==0){//queue is not free, server is free
+               q1.pushQueue(packet);
+               qin[packet]=times;
+               popped=q1.popQueue();
+               qout[popped]=times;
+               server=1;
+               sysout[popped]=qout[popped]+srt[popped];
+               lastsysout=sysout[packet];
+            }
+            else{ // queue and server are not free
+                q1.pushQueue(packet);
+                qin[packet]=times; 
+            }     
+        }       
+    }
+    for(int i=0;i<=packet;i++){
+        cout<<"packet: "<<i<<" Qin :"<<qin[i]<<" Qout :"<<qout[i]<<" iat:"<<iat[i]<<" srt: "<<srt[i]<<" sysout:"<<sysout[i]<<endl;
+    }
+    
     return 0;
 }
 
